19fcbb3cf384f2ec4d5f4a9ba5f537f888a6547c
SHUVO HALDER <42088593+cybsam@users.noreply.github.com>
2024-11-09 03:15:57 +0600

    header and nav

---
+++ TailwindFirst/tailwind.config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                headerbg: '#282828',
+                primaryRed: '#F40404',
+                grayCol: '#4e4e4e',
+            },
+            maxWidth: {
+                'container': '1144px',
+            },
+            fontFamily: {
+                'poppins': '"Poppins", sans-serif',
+                
+              },
+            backgroundImage:{
+                'banner': "url('image/bannerImages.jpg')",
+            }
+        }
+    }
+}
+// font-family: "Poppins", sans-serif;
